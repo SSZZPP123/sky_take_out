c72a20ed1257d202a0a25fa6b36ebbc182bad6d4
ZiPeng <2101657392@qq.com>
2023-10-24 23:37:57 +0800

    微信小程序菜品展示

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -44,10 +44,10 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
-        registry.addInterceptor(jwtTokenUserInterceptor)
+        /*registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
-                .excludePathPatterns("/user/shop/admin");
+                .excludePathPatterns("/user/shop/status");*/
     }
 
     /**
